d418c78c2a7c46821609d78995436b77ff8dd6aa
reich452 <reich452@regis.edu>
2017-06-19 16:35:52 -0600

    Added cloudKitManager

---
+++ 12AMCloudKit/CloudKitManager.swift
@@ -0,0 +1,389 @@
+//
+//  CloudKitManager.swift
+//  12AMCloudKit
+//
+//  Created by Nick Reichard on 6/19/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CloudKit
+
+private let CreatorUserRecordIDKey = "creatorUserRecordID"
+private let CreationDateKey = "creationDate"
+
+
+class CloudKitManager {
+    
+    static let shared = CloudKitManager()
+    let publicDatabase = CKContainer.default().publicCloudDatabase
+    
+    enum CloudKitTypes: String {
+        case userType = "User"
+    }
+    
+    
+    // MARK: - Fetch Records
+    
+    
+    // Give us everyting
+    // To fetch information from cloudKit we run a Quiery. Can be: Give me all of the objects with this type or it can be very specific using predicates. Quiery Operations - lets us handel multiple objects.
+    func fetchRecordsWithType(_ type: String,
+                              predicate: NSPredicate = NSPredicate(value: true),
+                              recordFetchedBlock: ((_ record: CKRecord) -> Void)?,
+                              completion: ((_ records: [CKRecord]?, _ error: Error?) -> Void)?) {
+        
+        var fetchedRecords: [CKRecord] = []
+        
+        let query = CKQuery(recordType: type, predicate: predicate)
+        let queryOperation = CKQueryOperation(query: query)
+        
+        let perRecordBlock = { (fetchedRecord: CKRecord) -> Void in
+            fetchedRecords.append(fetchedRecord) // passes the record
+            recordFetchedBlock?(fetchedRecord)
+        }
+        queryOperation.recordFetchedBlock = perRecordBlock
+        
+        var queryCompletionBlock: (CKQueryCursor?, Error?) -> Void = { (_, _) in }
+        
+        queryCompletionBlock = { (queryCursor: CKQueryCursor?, error: Error?) -> Void in
+            
+            if let queryCursor = queryCursor {
+                // there are more results, go fetch them
+                
+                let continuedQueryOperation = CKQueryOperation(cursor: queryCursor)
+                continuedQueryOperation.recordFetchedBlock = perRecordBlock
+                continuedQueryOperation.queryCompletionBlock = queryCompletionBlock
+                
+                self.publicDatabase.add(continuedQueryOperation)
+                
+            } else {
+                completion?(fetchedRecords, error)
+            }
+        }
+        queryOperation.queryCompletionBlock = queryCompletionBlock
+        
+        self.publicDatabase.add(queryOperation)
+    } // Bring back all the records for the specified type
+    
+    // MARK: - User Info Discovery
+    
+    func fetchLoggedInUserRecord(_ completion: ((_ record: CKRecord?, _ error: Error? ) -> Void)?) {
+        CKContainer.default().fetchUserRecordID { (recordID, error) in
+            
+            if let error = error, let completion = completion {
+                completion(nil, error)
+            }
+            
+            if let recordID = recordID, let completion = completion {
+                self.fetchRecord(withID: recordID, completion: completion)
+            }
+        }
+    }
+    
+    /// Apple gives you their first & last name for free.
+    func fetchUsername(for recordID: CKRecordID,
+                       completion: @escaping ((_ givenName: String?, _ familyName: String?) -> Void) = { _,_ in }) {
+        
+        let recordInfo = CKUserIdentityLookupInfo(userRecordID: recordID)
+        let operation = CKDiscoverUserIdentitiesOperation(userIdentityLookupInfos: [recordInfo])
+        
+        var userIdenties = [CKUserIdentity]()
+        operation.userIdentityDiscoveredBlock = { (userIdentity, _) in
+            userIdenties.append(userIdentity)
+        }
+        operation.discoverUserIdentitiesCompletionBlock = { (error) in
+            if let error = error {
+                NSLog("Error getting username from record ID: \(error)")
+                completion(nil, nil)
+                return
+            }
+            
+            let nameComponents = userIdenties.first?.nameComponents
+            completion(nameComponents?.givenName, nameComponents?.familyName)
+        }
+        
+        CKContainer.default().add(operation)
+    }
+    
+    /// Only fetches with your contacts
+    func fetchAllDiscoverableUsers(completion: @escaping ((_ userInfoRecords: [CKUserIdentity]?) -> Void) = { _ in }) {
+        
+        let operation = CKDiscoverAllUserIdentitiesOperation()
+        
+        var userIdenties = [CKUserIdentity]()
+        operation.userIdentityDiscoveredBlock = { userIdenties.append($0) }
+        operation.discoverAllUserIdentitiesCompletionBlock = { error in
+            if let error = error {
+                NSLog("Error discovering all user identies: \(error)")
+                completion(nil)
+                return
+            }
+            
+            completion(userIdenties)
+        }
+        
+        CKContainer.default().add(operation)
+    }
+    
+    func fetchRecord(withID recordID: CKRecordID, completion: ((_ record: CKRecord?, _ error: Error?) -> Void)?) {
+        
+        publicDatabase.fetch(withRecordID: recordID) { (record, error) in
+            
+            completion?(record, error)
+        }
+    } // Bring back the reocord for whatever item has that ID
+    
+    
+    func fetchCurrentUserRecords(_ type: String, completion: ((_ records: [CKRecord]?, _ error: Error?) -> Void)?) {
+        
+        fetchLoggedInUserRecord { (record, error) in
+            
+            if let record = record {
+                
+                let predicate = NSPredicate(format: "%K == %@", argumentArray: [CreatorUserRecordIDKey, record.recordID])
+                
+                self.fetchRecordsWithType(type, predicate: predicate, recordFetchedBlock: nil, completion: completion)
+            }
+        }
+    } // Retrieve the logged in user's record, and then all of the records that belong to that specific user --> Get Finn's record and then get all of the records associated with him (gun record, helmet record, etc.)
+    
+    func fetchRecordsFromDateRange(_ type: String, fromDate: Date, toDate: Date, completion: ((_ records: [CKRecord]?, _ error: Error?) -> Void)?) {
+        
+        let startDatePredicate = NSPredicate(format: "%K > %@", argumentArray: [CreationDateKey, fromDate])
+        let endDatePredicate = NSPredicate(format: "%K < %@", argumentArray: [CreationDateKey, toDate])
+        let predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [startDatePredicate, endDatePredicate])
+        
+        self.fetchRecordsWithType(type, predicate: predicate, recordFetchedBlock: nil) { (records, error) in
+            
+            completion?(records, error)
+        }
+    } // Get all the records that were created between a start and end date (if you want the old stormtrooper helmet records, you might use this)
+    
+    // MARK: - Save and Modify
+    
+    func saveRecord(_ record: CKRecord, completion: ((_ record: CKRecord?, _ error: Error?) -> Void)?) {
+        
+        publicDatabase.save(record, completionHandler: { (record, error) in
+            
+            completion?(record, error)
+        })
+    }
+    
+    func saveRecords(_ records: [CKRecord], perRecordCompletion: ((_ record: CKRecord?, _ error: Error?) -> Void)?, completion: ((_ records: [CKRecord]?, _ error: Error?) -> Void)?) {
+        
+        modifyRecords(records, perRecordCompletion: perRecordCompletion, completion: completion)
+    }
+    
+    func modifyRecords(_ records: [CKRecord], perRecordCompletion: ((_ record: CKRecord?, _ error: Error?) -> Void)?, completion: ((_ records: [CKRecord]?, _ error: Error?) -> Void)?) {
+        
+        let operation = CKModifyRecordsOperation(recordsToSave: records, recordIDsToDelete: nil)
+        operation.savePolicy = .changedKeys
+        operation.queuePriority = .high
+        operation.qualityOfService = .userInteractive
+        
+        operation.perRecordCompletionBlock = perRecordCompletion
+        
+        operation.modifyRecordsCompletionBlock = { (records, recordIDs, error) -> Void in
+            if let error = error { print(error.localizedDescription) }
+            (completion?(records, error))!
+        }
+        
+        publicDatabase.add(operation)
+    }
+    
+    
+    // MARK: - Delete
+    
+    func deleteRecordWithID(_ recordID: CKRecordID, completion: ((_ recordID: CKRecordID?, _ error: Error?) -> Void)?) {
+        
+        publicDatabase.delete(withRecordID: recordID) { (recordID, error) in
+            completion?(recordID, error)
+        }
+    } // The record with the specified will be DESTROYED
+    
+    func deleteRecordsWithID(_ recordIDs: [CKRecordID], completion: ((_ records: [CKRecord]?, _ recordIDs: [CKRecordID]?, _ error: Error?) -> Void)?) {
+        
+        let operation = CKModifyRecordsOperation(recordsToSave: nil, recordIDsToDelete: recordIDs)
+        operation.savePolicy = .ifServerRecordUnchanged
+        
+        operation.modifyRecordsCompletionBlock = completion
+        
+        publicDatabase.add(operation)
+    } // Find the younglings for these IDs, and kill their records
+    
+    
+    // MARK: - Subscriptions
+    
+    func subscribe(_ type: String,
+                   predicate: NSPredicate,
+                   subscriptionID: String,
+                   contentAvailable: Bool,
+                   alertBody: String? = nil,
+                   desiredKeys: [String]? = nil,
+                   options: CKQuerySubscriptionOptions,
+                   completion: ((_ subscription: CKSubscription?, _ error: Error?) -> Void)?) {
+        
+        let subscription = CKQuerySubscription(recordType: type, predicate: predicate, subscriptionID: subscriptionID, options: options)
+        
+        let notificationInfo = CKNotificationInfo()
+        notificationInfo.alertBody = alertBody
+        notificationInfo.shouldSendContentAvailable = contentAvailable
+        notificationInfo.desiredKeys = desiredKeys
+        
+        subscription.notificationInfo = notificationInfo
+        
+        publicDatabase.save(subscription, completionHandler: { (subscription, error) in
+            
+            completion?(subscription, error)
+        })
+    }
+    
+    func unsubscribe(_ subscriptionID: String, completion: ((_ subscriptionID: String?, _ error: Error?) -> Void)?) {
+        
+        publicDatabase.delete(withSubscriptionID: subscriptionID) { (subscriptionID, error) in
+            
+            completion?(subscriptionID, error)
+        }
+    }
+    
+    /// if you delete the app and re-download it. re-links your phone with cloudKit
+    func fetchSubscriptions(_ completion: ((_ subscriptions: [CKSubscription]?, _ error: Error?) -> Void)?) {
+        
+        publicDatabase.fetchAllSubscriptions { (subscriptions, error) in
+            
+            completion?(subscriptions, error)
+        }
+    }
+    
+    func fetchSubscription(_ subscriptionID: String, completion: ((_ subscription: CKSubscription?, _ error: Error?) -> Void)?) {
+        
+        publicDatabase.fetch(withSubscriptionID: subscriptionID) { (subscription, error) in
+            
+            completion?(subscription, error)
+        }
+    }
+    
+    // MARK: ------ NOT Using These Methods below. Good to have the for future. -------
+    
+    // MARK: - CloudKit Permissions
+    
+    /// Checks if they have a iCloud account
+    func checkCloudKitAvailability() {
+        
+        CKContainer.default().accountStatus() {
+            (accountStatus:CKAccountStatus, error:Error?) -> Void in
+            
+            switch accountStatus {
+            case .available:
+                print("CloudKit available. Initializing full sync.")
+                return
+            default:
+                self.handleCloudKitUnavailable(accountStatus, error: error)
+            }
+        }
+    }
+    
+    func handleCloudKitUnavailable(_ accountStatus: CKAccountStatus, error:Error?) {
+        
+        var errorText = "Synchronization is disabled\n"
+        if let error = error {
+            print("handleCloudKitUnavailable ERROR: \(error)")
+            print("An error occured: \(error.localizedDescription)")
+            errorText += error.localizedDescription
+        }
+        
+        switch accountStatus {
+        case .restricted:
+            errorText += "iCloud is not available due to restrictions"
+        case .noAccount:
+            errorText += "There is no CloudKit account setup.\nYou can setup iCloud in the Settings app."
+        default:
+            break
+        }
+        
+        displayCloudKitNotAvailableError(errorText)
+    }
+    
+    func displayCloudKitNotAvailableError(_ errorText: String) {
+        
+        DispatchQueue.main.async(execute: {
+            
+            let alertController = UIAlertController(title: "iCloud Synchronization Error", message: errorText, preferredStyle: .alert)
+            
+            let dismissAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil);
+            
+            alertController.addAction(dismissAction)
+            
+            if let appDelegate = UIApplication.shared.delegate,
+                let appWindow = appDelegate.window!,
+                let rootViewController = appWindow.rootViewController {
+                rootViewController.present(alertController, animated: true, completion: nil)
+            }
+        })
+    }
+    
+    // MARK: - CloudKit Discoverability
+    
+    /// Only useful for a family. Can't find friens
+    func requestDiscoverabilityPermission() {
+        
+        CKContainer.default().status(forApplicationPermission: .userDiscoverability) { (permissionStatus, error) in
+            
+            if permissionStatus == .initialState {
+                CKContainer.default().requestApplicationPermission(.userDiscoverability, completionHandler: { (permissionStatus, error) in
+                    
+                    self.handleCloudKitPermissionStatus(permissionStatus, error: error)
+                })
+            } else {
+                
+                self.handleCloudKitPermissionStatus(permissionStatus, error: error)
+            }
+        }
+    }
+    
+    func handleCloudKitPermissionStatus(_ permissionStatus: CKApplicationPermissionStatus, error:Error?) {
+        
+        if permissionStatus == .granted {
+            print("User Discoverability permission granted. User may proceed with full access.")
+        } else {
+            var errorText = "Synchronization is disabled\n"
+            if let error = error {
+                print("handleCloudKitUnavailable ERROR: \(error)")
+                print("An error occured: \(error.localizedDescription)")
+                errorText += error.localizedDescription
+            }
+            
+            switch permissionStatus {
+            case .denied:
+                errorText += "You have denied User Discoverability permissions. You may be unable to use certain features that require User Discoverability."
+            case .couldNotComplete:
+                errorText += "Unable to verify User Discoverability permissions. You may have a connectivity issue. Please try again."
+            default:
+                break
+            }
+            
+            displayCloudKitPermissionsNotGrantedError(errorText)
+        }
+    }
+    
+    func displayCloudKitPermissionsNotGrantedError(_ errorText: String) {
+        
+        DispatchQueue.main.async(execute: {
+            
+            let alertController = UIAlertController(title: "CloudKit Permissions Error", message: errorText, preferredStyle: .alert)
+            
+            let dismissAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil);
+            
+            alertController.addAction(dismissAction)
+            
+            if let appDelegate = UIApplication.shared.delegate,
+                let appWindow = appDelegate.window!,
+                let rootViewController = appWindow.rootViewController {
+                rootViewController.present(alertController, animated: true, completion: nil)
+            }
+        })
+    }
+    
+}
---
+++ 12AMCloudKit/UserController.swift
@@ -0,0 +1,41 @@
+//
+//  UserController.swift
+//  12AMCloudKit
+//
+//  Created by Nick Reichard on 6/19/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CloudKit
+
+class UserController {
+    
+    static let shared = UserController()
+    
+    let publicDB = CKContainer.default().publicCloudDatabase
+    let privateDB = CKContainer.default().privateCloudDatabase
+    
+    var appleUserRecordID: CKRecordID?
+    var blockUserRef: [CKReference]? = []
+    var users: [User] = []
+    var dict : [String: AnyObject]?
+    // more effieceint when you want to find a user
+    var currentUser: User?
+    
+    // MARK: - CRUD
+    
+    func fetchCurrentUser(completion: @escaping (User?) -> Void) {
+        CKContainer.default().fetchUserRecordID { (appleUserRecordID, error) in
+            if let error = error { print("Error fetching user: \(error.localizedDescription)") }
+            guard let appleUserRecordID = appleUserRecordID else { return }
+            let appleUserRef = CKReference(recordID: appleUserRecordID, action: .none)
+            let predicate = NSPredicate(format: "appleUserRef == %@", appleUserRef)
+            let query = CKQuery(recordType: "User", predicate: predicate)
+            
+            
+            
+        }
+    }
+}
